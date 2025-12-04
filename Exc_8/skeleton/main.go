46189fba9f9ee20dd2fce73a1afa127713419870
AndreasRanzmaier <zmoa.dev@gmail.com>
2025-12-04 19:31:50 +0100

    starting the gRPC server in main

--- Exc_8/skeleton/main.go
+++ Exc_8/skeleton/main.go
@@ -1,14 +1,28 @@
 package main
 
 import (
+	"exc8/client"
+	"exc8/server"
+	"log"
 	"time"
 )
 
 func main() {
 	go func() {
-		// todo start server
+		if err := server.StartGrpcServer(); err != nil {
+			log.Fatalf("failed to start server: %v", err)
+		}
 	}()
+	// Give him a second to start
 	time.Sleep(1 * time.Second)
-	// todo start client
+
+	c, err := client.NewGrpcClient()
+	if err != nil {
+		log.Fatalf("failed to create client: %v", err)
+	}
+	if err := c.Run(); err != nil {
+		log.Fatalf("failed to run client: %v", err)
+	}
+
 	println("Orders complete!")
 }

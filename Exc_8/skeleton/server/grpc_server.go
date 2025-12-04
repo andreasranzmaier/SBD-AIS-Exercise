7575fb211b40deb105d45f6d09624fc6267b67c1
AndreasRanzmaier <zmoa.dev@gmail.com>
2025-12-04 19:18:18 +0100

    adding the server instancen in the create functon and returnig it served

--- Exc_8/skeleton/server/grpc_server.go
+++ Exc_8/skeleton/server/grpc_server.go
@@ -4,8 +4,8 @@ import (
 	"context"
 	"exc8/pb"
 	"fmt"
-	"log/slog"
 	"net"
+	"sync"
 
 	"google.golang.org/grpc"
 	emptypb "google.golang.org/protobuf/types/known/emptypb"
@@ -14,13 +14,23 @@ import (
 
 type GRPCService struct {
 	pb.UnimplementedOrderServiceServer
+	drinks []*pb.Drink
+	orders []*pb.Order
+	mu     sync.Mutex // locking when accessing orders
 }
 
 func StartGrpcServer() error {
 	// Create a new gRPC server.
 	srv := grpc.NewServer()
 	// Create grpc service
-	grpcService := &GRPCService{}
+	grpcService := &GRPCService{
+		drinks: []*pb.Drink{ // predefined drinks as list of pb.Drink
+			{Id: 1, Name: "Spritzer", Price: 2.0, Description: "Wine with soda"},
+			{Id: 2, Name: "Beer", Price: 3.0, Description: "Hagenberger Gold"},
+			{Id: 3, Name: "Coffee", Price: 2.5, Description: "Mifare isn't that secure"},
+		},
+		orders: []*pb.Order{}, // start with empty orders
+	}
 	// Register our service implementation with the gRPC server.
 	pb.RegisterOrderServiceServer(srv, grpcService)
 	// Serve gRPC server on port 4000.
@@ -28,11 +38,5 @@ func StartGrpcServer() error {
 	if err != nil {
 		return err
 	}
-	err = srv.Serve(lis)
-	if err != nil {
-		return err
-	}
-	return nil
+	return srv.Serve(lis)
 }
-
-// todo implement functions
